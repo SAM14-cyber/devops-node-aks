bb9080f387b124fd064d3d92a0182a3e8a55a05d
sam <samj22@karunya.edu.in>
2025-10-30 03:37:21 +0530

    Added GitHub Actions CI/CD workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,13 +9,15 @@ terraform {
 
 provider "docker" {}
 
+# Pull image from Docker Hub (your repo: samj4/devops-node-aks)
 resource "docker_image" "app_image" {
-  name = var.image_name
+  name = "samj4/devops-node-aks:latest"
 }
 
+# Run container
 resource "docker_container" "app_container" {
-  name  = "devops-node-aks"
-  image = docker_image.app_image.latest
+  name  = "devops-container"
+  image = docker_image.app_image.name
   ports {
     internal = 8080
     external = 8080
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,4 @@
 variable "image_name" {
   description = "Docker image name"
-  default     = "sam14/devops-node-aks:latest"
+  default     = "samj4/devops-node-aks:latest"
 }
